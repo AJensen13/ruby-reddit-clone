30f6cd830c28bcab425162d1cbd1de980ff91e8d
Andrew Jensen <andrew@clevercoding.com>
2018-12-06 10:45:31 -0700

    added routes and form to add boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,21 @@
 class BoardsController < ApplicationController
+	def show
+		@board = Board.find(params[:id])
+	end
+
 	def new
 		
+	end
+
+	def create
+		@board = Board.new(board_params)
+
+		@board.save
+		redirect_to @board
+	end
+
+	private
+	def board_params
+		params.require(:board).permit(:title, :text)
 	end
 end
